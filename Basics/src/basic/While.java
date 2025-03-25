651ac5cb2a594ea029640f214db73c78d59acfcf
NIraj Balpande <nirajbalpande12@gmail.com>
2025-03-25 23:32:11 +0530

    WHILE LOOP

---
+++ Basics/src/basic/While.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int t=10;
+		while (t!=0) {
+			System.out.println("Hello");
+			t--;
+			
+		}
+
+	}
+
+}
