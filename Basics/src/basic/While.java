70f350cd3f890d0c070ca21c5dae51e2ef9274e7
NIraj Balpande <nirajbalpande12@gmail.com>
2025-03-29 22:58:00 +0530

    Multiples Of n

--- Basics/src/basic/While.java
+++ Basics/src/basic/While.java
@@ -4,11 +4,18 @@ public class While {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int t=10;
-		while (t!=0) {
-			System.out.println("Hello");
-			t--;
-			
+//		int t=10;
+//		while (t!=0) {
+//			System.out.println("Hello");
+//			t--;
+//			
+//		}
+		//print multiples of n
+		int n=2;
+		int i=1;
+		while (i<=10) {
+			System.out.println(n*i);
+			i++;
 		}
 
 	}
